7dac34ac7fe3d9193521e5565b58e5de8c6328ad
stevleibelt <stev@leibelt.de>
2014-04-18 22:56:59 +0200

    added rest_with_propel

---
+++ php/rest_with_propel/script/build-propel.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+####
+# @author stev leibelt <artodeto@bazzline.net>
+# @since 2014-03-27
+####
+
+# moving to project root
+SCRIPT_PATH=$(cd $(dirname "$0"); pwd)
+cd "$SCRIPT_PATH/../configuration/propel"
+
+# builind propel
+sh "$SCRIPT_PATH/../vendor/bin/propel-gen"
+
+exit 0
